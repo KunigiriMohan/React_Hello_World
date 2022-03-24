42fbded44f919719b354ea08e7cbc71a82ae88f7
Mohan Kunigiri <kmohanr5@gmail.com>
2022-03-24 14:32:35 +0530

    Added : Filling with Text messages.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import logo from './assets/img/Bridgelab.png'
 
 
 class App extends React.Component{
-  userName=''
   webPage='https://www.bridgelabz.com/'
   
   onClick = ($event) =>{
@@ -34,17 +33,33 @@ class App extends React.Component{
 
   render(){
     return(
-      <>
+      <body>
+        <div class='main'>
       <div>
         <h1>Hello {this.state.userName} from BridgeLabz using variable</h1>
-        <h2>Hello {this.title1} from BridgeLabz using variable</h2>
         <img src={logo} onClick={this.onClick} alt='The BridgeLabz Logo: a Bridge to Employment through lab works'></img>
       </div>
       <div>
         <input class='input' onChange={this.onNameChange}></input>
         <span className='error-output'>{this.state.nameError}</span>
       </div>
-      </> 
+      <p>At Bridgelabz ,we re a techie commuminity of </p>
+      <ol>
+        <li>Technologies</li>
+        <li>Thinkers</li>
+        <li>Builders</li>
+      </ol>
+      <p>
+          Working together to keep the Tech Emolybility of engineers alive and accesible,
+          so TechCompanies woe=rldwode can get contributers and creators for Technology Solutions.
+          We believe this act of human collabration across an employability platform is essential
+          to individual growth and our collective future.
+      </p>
+      <p>To know about us , visit <a href="https://www.bridgelabz.com/" >Bridgelab</a>
+        to learn even more about our mission i,e. <strong>Employibility to all</strong>
+      </p>
+      </div>
+      </body> 
     );
   }
 }
